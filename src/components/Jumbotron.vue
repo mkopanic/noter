bcb329db1f9c7e3c8cf1a6d4b7a1ab0a44bc7ae9
Michal Kopanicak <43612845+mkopanic@users.noreply.github.com>
2018-10-11 19:35:26 -0400

    Update Jumbotron.vue

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,6 +1,6 @@
 <template>
   <b-jumbotron header="Noter instance" lead="A minimalistic note taking webapp" >
-    <p>Copyright of Hayden Hoffman 2018</p>
+    <p>Copyright of Michal Kopanicak 2018</p>
   </b-jumbotron>
 </template>
 
